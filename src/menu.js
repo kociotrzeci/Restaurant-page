8ff375c6e309a59b3af366f4ab91e5922aeffe02
kociotrzeci <80517383+kociotrzeci@users.noreply.github.com>
2024-01-31 21:53:05 +0100

    menu page styling

--- src/menu.js
+++ src/menu.js
@@ -8,7 +8,7 @@ function dish(imageAdress, name, textContent) {
     image.src = (imageAdress);
     const header = document.createElement('h1');
     header.textContent = name;
-    const text = document.createElement('p1');
+    const text = document.createElement('p');
     text.className = 'dish-text';
     text.textContent = textContent;
     textContainer.appendChild(header);
@@ -24,9 +24,9 @@ function dish(imageAdress, name, textContent) {
 function menu() {
     const page = document.createElement('div');
     page.id = 'menu';
-    page.appendChild(dish(" ", 'kaszanka', 'bardzo dobrrra kaszanka'));
-    page.appendChild(dish(" ", 'kaszanka', 'bardzo dobrrra kaszanka'));
-    page.appendChild(dish(" ", 'kaszanka', 'bardzo dobrrra kaszanka'));
+    page.appendChild(dish(" ", 'Some Kaszanka', 'Juicy kashanka from the oven, served with onion and apples, seasoned with horseradish and cinnamon'));
+    page.appendChild(dish(" ", 'Other Kaszanka', 'Crispy kashanka from the pan, mixed with caramelized onion, salt and pepper. Perfect with bread or fried egg'));
+    page.appendChild(dish(" ", 'Kaszanka la bamba', 'Simple and delicious kashanka from the oven, drizzled with melted butter and sprinkled with horseradish. Tastes great with potatoes or salad'));
     return (page);
 }
 
